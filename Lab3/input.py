3a02e4887a8a385e9dcf9da1d7649f3f17df77bf
Jafrips <64084481+Jafrips@users.noreply.github.com>
2024-06-11 10:14:29 +0800

    Update input.py

--- Lab3/input.py
+++ Lab3/input.py
@@ -20,17 +20,21 @@ generations = 50  # количество поколений
 
 #### Проверки ####
 
+# Окрашивание текста ошибок в консоли красным
+from colorama import init, Fore
+init()
+
 # Проверка min_exercises_per_day и max_exercises_per_day
 if min_exercises_per_day > max_exercises_per_day:
-    raise ValueError("Минимальное количество упражнений в день не может быть больше максимального, проверьте файл input.py")
+    raise ValueError(Fore.RED + "Минимальное количество упражнений в день не может быть больше максимального, проверьте файл input.py")
 elif max_exercises_per_day < min_exercises_per_day:
-    raise ValueError("Максимальное количество упражнений в день не может быть меньше минимального, проверьте файл input.py")
+    raise ValueError(Fore.RED + "Максимальное количество упражнений в день не может быть меньше минимального, проверьте файл input.py")
 
 for muscle_group, exercises in weekly_exercise_plan.items():
     if exercises < 0 or exercises > 5:
-        raise ValueError("Количество упражнений на каждую группу мышц должно быть в диапазоне от 0 до 5. Проверьте значения в файле input.py")
+        raise ValueError(Fore.RED + "Количество упражнений на каждую группу мышц должно быть в диапазоне от 0 до 5. Проверьте значения в файле input.py")
     
 totalEX = sum(weekly_exercise_plan.values())
 availableEX = max_exercises_per_day * 7
 if totalEX > availableEX:
-    raise ValueError(f"Вы пытаетесь распределить {totalEX} упражнений, когда возможно наличие только {availableEX} упражнений за неделю. Пересмотрите значение максимального возможного количества упражнений в тренировочный день или количество упражнений на каждую группу мышц. Файл input.py")
+    raise ValueError(Fore.RED + f"Вы пытаетесь распределить {totalEX} упражнений, когда возможно наличие только {availableEX} упражнений за неделю. Пересмотрите значение максимального возможного количества упражнений в тренировочный день или количество упражнений на каждую группу мышц. Файл input.py")
